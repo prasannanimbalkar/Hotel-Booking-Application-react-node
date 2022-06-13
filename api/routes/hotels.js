0f3a030e32afc6c79c353c06c33238672967e911
Prasanna Nimbalkar <prasannanimbalkar@Prasannas-MacBook-Pro.local>
2022-06-13 18:17:25 -0400

    modified hotel routes and model

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -3,5 +3,22 @@ import express from "express"
 const router = express.Router();
 
 
+//CREATE
+router.post("/", verifyAdmin, createHotel);
+
+//UPDATE
+router.put("/:id", verifyAdmin, updateHotel);
+//DELETE
+router.delete("/:id", verifyAdmin, deleteHotel);
+//GET
+
+router.get("/find/:id", getHotel);
+//GET ALL
+
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);s
+
 
 export default router
