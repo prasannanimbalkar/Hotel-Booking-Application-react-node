d941a907909b3d30d4323b6e7118789d1da12165
Prasanna Nimbalkar <prasannanimbalkar@Prasannas-MacBook-Pro.local>
2022-06-13 03:19:37 -0400

    rearranging files and adding response to api

--- api/index.js
+++ api/index.js
@@ -5,8 +5,10 @@ import mongoose from "mongoose"
 
 const app = express()
 
+// a lightweight npm package that automatically loads environment variables from a . env file into the process. env object.
 dotenv.config()
 
+// connecting mongodb using mongoose
 const connect = async () => {
 
     try {
@@ -16,6 +18,12 @@ const connect = async () => {
         throw error;
     }
 }
+
+// express takes req and res parameters and use it inside any api request
+// user makes req and res sent by us
+app.get('/', (req, res) => {
+    res.send("this is first request")
+})
 
 mongoose.connection.on("disconnected", () => {
     console.log("mongoDB disconnected")
