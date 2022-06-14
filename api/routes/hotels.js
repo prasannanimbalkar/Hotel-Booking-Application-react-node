bd16ec4802d0327112b5fcb3e9f3f5e0706502b6
Prasanna Nimbalkar <prasannanimbalkar@Prasannas-MacBook-Pro.local>
2022-06-14 05:33:02 -0400

    modifying routes to controllers

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,12 @@
 import express from "express"
 import Hotel from "../models/Hotel.js"
+import {
+    createHotel,
+    deleteHotel,
+    getHotel,
+    getHotels,
+    updateHotel,
+  } from "../controllers/hotel.js";
 
 const router = express.Router();
 
@@ -8,58 +15,72 @@ const router = express.Router();
 
 //CREATE
 // we use async as we are trying to connect to db, try to create document and try to create colection which taks time
-router.post("/", async (req, res) => {
 
-    const newHotel = new Hotel(req.body) 
 
-    try {
-        const savedHotel = await newHotel.save()
-        res.status(200).json(savedHotel)
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+// router.post("/", async (req, res) => {
+//     const newHotel = new Hotel(req.body) 
+//     try {
+//         const savedHotel = await newHotel.save()
+//         res.status(200).json(savedHotel)
+//     }catch(err){
+//         res.status(500).json(err)
+//     }
+// })
+
+router.post("/", createHotel )
+
+
 
 // //UPDATE
-router.put("/:id", async (req, res) => {
-    try {
-        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true})
-        res.status(200).json(updatedHotel)
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+// router.put("/:id", async (req, res) => {
+//     try {
+//         const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true})
+//         res.status(200).json(updatedHotel)
+//     }catch(err){
+//         res.status(500).json(err)
+//     }
+// })
+
+router.put("/:id", updateHotel)
+
 
 // //DELETE
-router.delete("/:id", async (req, res) => {
-    try {
-        await Hotel.findByIdAndDelete(req.params.id)
-        res.status(200).json("Hotel has been deleted")
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+// router.delete("/:id", async (req, res) => {
+//     try {
+//         await Hotel.findByIdAndDelete(req.params.id)
+//         res.status(200).json("Hotel has been deleted")
+//     }catch(err){
+//         res.status(500).json(err)
+//     }
+// })
+
+router.delete("/:id", deleteHotel)
+
 
 // //GET
-router.get("/:id", async (req, res) => {
-    try {
-        const hotel = await Hotel.findById(req.params.id)
-        res.status(200).json(hotel)
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+// router.get("/:id", async (req, res) => {
+//     try {
+//         const hotel = await Hotel.findById(req.params.id)
+//         res.status(200).json(hotel)
+//     }catch(err){
+//         res.status(500).json(err)
+//     }
+// })
+
+router.get("/:id", getHotel)
+
 
 // //GET ALL
-router.get("/", async (req, res,next) => {
-    
-    try {
-        const hotel = await Hotel.find()
-        res.status(200).json(hotel)
-    }catch(err){
-        next(err)
-    }
-})
+// router.get("/", async (req, res,next) => {
+//     try {
+//         const hotel = await Hotel.find()
+//         res.status(200).json(hotel)
+//     }catch(err){
+//         next(err)
+//     }
+// })
+
+router.get("/", getHotels)
 
 
 // router.get("/", getHotels);
